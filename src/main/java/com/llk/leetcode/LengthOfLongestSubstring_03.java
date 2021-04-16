b26a8734d2b6c6cf4a99bd647bb49abdf99a8774
liliangkai <413977740@qq.com>
2021-04-16 17:44:28 +0800

    LengthOfLongestSubstring

--- src/main/java/com/llk/leetcode/LengthOfLongestSubstring_03.java
+++ src/main/java/com/llk/leetcode/LengthOfLongestSubstring_03.java
@@ -1,5 +1,10 @@
 package com.llk.leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 无重复字符的最长子串
  * 滑动窗口
@@ -9,7 +14,38 @@ package com.llk.leetcode;
  */
 public class LengthOfLongestSubstring_03 {
 
-    public int lengthOfLongestSubstring(String s) {
-        return 1;
+    public int lengthOfLongestSubstring_0(String s) {
+        Set<Character> set = new HashSet<>();
+        int n = s.length();
+        int rightPoint = -1;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            while (rightPoint + 1 < n && !set.contains(s.charAt(rightPoint + 1))) {
+                rightPoint++;
+                set.add(s.charAt(rightPoint));
+            }
+            ans = Math.max(ans, rightPoint - i + 1);
+        }
+        return ans;
+    }
+
+    public int lengthOfLongestSubstring_1(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int leftPoint = 0;
+        int max = 0;
+        for (int rightPoint = 0; rightPoint < s.length(); rightPoint++) {
+            char c = s.charAt(rightPoint);
+            if (map.containsKey(c)) {
+                // max函数针对leftpoint在中间，而发生重复的值小于leftpoint的情况
+                leftPoint = Math.max(map.get(c) + 1, leftPoint);
+            }
+            max = Math.max(max, rightPoint - leftPoint + 1);
+            // 如果有一样的char会覆盖掉原来的
+            map.put(c, rightPoint);
+        }
+        return max;
     }
 }
--- src/test/java/com/llk/leetcode/LengthOfLongestSubstring_03Test.java
+++ src/test/java/com/llk/leetcode/LengthOfLongestSubstring_03Test.java
@@ -1,6 +1,14 @@
 package com.llk.leetcode;
 
 
-class LengthOfLongestSubstring_03Test {
+import org.junit.jupiter.api.Test;
 
+class LengthOfLongestSubstring_03Test {
+    @Test
+    void test1() {
+        String s = "abcabcbb";
+        LengthOfLongestSubstring_03 lengthOfLongestSubstring_03 = new LengthOfLongestSubstring_03();
+        int i = lengthOfLongestSubstring_03.lengthOfLongestSubstring_1(s);
+        System.out.println(i);
+    }
 }
