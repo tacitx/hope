c81c0575fc940e2d760e729c2b4bdbcc935009de
liliangkai <413977740@qq.com>
2021-05-03 10:21:04 +0800

    JUC

--- src/main/java/com/llk/thread/MyQueueImprove.java
+++ src/main/java/com/llk/thread/MyQueueImprove.java
@@ -5,14 +5,59 @@ package com.llk.thread;
  * @date 2021/5/2
  */
 public class MyQueueImprove extends MyQueue {
+    private String[] data = new String[10];
+    private int putIndex = 0;
+    private int getindex = 0;
+    private int size = 0;
 
     @Override
     public synchronized void put(String s) {
-        super.put(s);
+        if (size == data.length) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            put(s);
+        } else {
+            put0(s);
+            notify();
+        }
+
+
+    }
+
+    private void put0(String s) {
+        data[putIndex] = s;
+        size++;
+        putIndex++;
+        if (putIndex == data.length) {
+            putIndex = 0;
+        }
     }
 
     @Override
     public synchronized String get() {
-        return super.get();
+        if (size == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return get();
+        } else {
+            notify();
+            return get0();
+        }
+    }
+
+    private String get0() {
+        String result = data[getindex];
+        getindex++;
+        size--;
+        if (getindex == data.length) {
+            getindex = 0;
+        }
+        return result;
     }
 }
